0baea61e5cb6fd8b561c3a03ba653da34979d0d1
Rustam <tabarzin@gmail.com>
2023-08-18 14:28:00 +0300

    refactor: make general script file for hamburger and other shared code

--- pages/pets/script.js
+++ pages/pets/script.js
@@ -1,18 +1,5 @@
 import petsData from "../main/pets.json" assert { type: "json" };
 
-const hamburger = document.querySelector(".hamburger");
-const navBurger = document.querySelector(".nav");
-const menuLink = document.querySelector(".nav-list");
-
-function toggleMenu() {
-  hamburger.classList.toggle("open");
-  navBurger.classList.toggle("hide");
-  navBurger.classList.toggle("nav-burger");
-}
-
-hamburger.addEventListener("click", toggleMenu);
-menuLink.addEventListener("click", toggleMenu);
-
 // PETS GENERATOR //
 
 const modal = document.getElementById("modal");
---
+++ pages/script.js
@@ -0,0 +1,14 @@
+// HAMBURGER //
+
+const hamburger = document.querySelector(".hamburger");
+const navBurger = document.querySelector(".nav");
+const menuLink = document.querySelector(".nav-list");
+
+function toggleMenu() {
+  hamburger.classList.toggle("open");
+  navBurger.classList.toggle("hide");
+  navBurger.classList.toggle("nav-burger");
+}
+
+hamburger.addEventListener("click", toggleMenu);
+menuLink.addEventListener("click", toggleMenu);
